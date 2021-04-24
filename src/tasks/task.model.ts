f14b97805caa74d2463f366abf6c050b02047131
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-04-24 22:24:11 +0200

    refactor: add Task interface and TasksStatus enum, implemented to controller and service

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,14 @@
+interface Task {
+	id: string;
+	title: string;
+	description: string;
+	status: TasksStatus;
+}
+
+enum TasksStatus {
+	OPEN = "OPEN",
+	IN_PROGRESS = "IN_PROGRESS",
+	DONE = "DONE",
+}
+
+export { Task, TasksStatus };
