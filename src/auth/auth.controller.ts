f9cb3f8561ec0e2c13693e6d099b7bc0b7d067af
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-08 21:51:04 +0200

    feat/server: pipline with for sign up new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import AuthCredentialsDto from "./dto/auth-credentials.dto";
+import { AuthService } from "./auth.service";
 
 @Controller("auth")
-export class AuthController {}
+class AuthController {
+	constructor(private authService: AuthService) {}
+
+	@Post("/signup")
+	async signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+		return this.authService.signUp(authCredentialsDto);
+	}
+}
+
+export default AuthController;
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,24 @@
 import { EntityRepository, Repository } from "typeorm";
 import User from "./user.entity";
+import AuthCredentialsDto from "./dto/auth-credentials.dto";
+import { Logger } from "@nestjs/common";
 
 @EntityRepository(User)
-class UserRepository extends Repository<User> {}
+class UserRepository extends Repository<User> {
+	private logger = new Logger("UserRepository");
+
+	async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+		const { username, password } = authCredentialsDto;
+
+		const newUser = new User();
+		newUser.username = username;
+		newUser.password = password;
+
+		await newUser.save();
+
+		this.logger.log(`New user created ${username}`);
+		return;
+	}
+}
 
 export default UserRepository;
