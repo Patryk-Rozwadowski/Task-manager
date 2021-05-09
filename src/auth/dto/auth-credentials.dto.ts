8ece6df826b09478fe0029f3905bf02b17c3b89e
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-05-09 23:52:14 +0200

    feat/server: validation for signup

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,21 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+/* 	1 Upper case letter
+ 	1 Lower case letter
+ 	1 number or special character
+*/
+const authRegex = /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+
 class AuthCredentialsDto {
+	@IsString()
+	@MinLength(4)
+	@MaxLength(15)
 	username: string;
+
+	@IsString()
+	@MinLength(5)
+	@MaxLength(15)
+	@Matches(authRegex, { message: "Password too weak." })
 	password: string;
 }
 
