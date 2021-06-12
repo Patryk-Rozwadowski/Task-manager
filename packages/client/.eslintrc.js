5933141b1576d46ba6437662e5a0bba41c7dc4c4
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-06-13 01:26:41 +0200

    fix: linting works for client and server

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "standard", "../../.eslintrc.json"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+   env: {
+      browser: true,
+      es2021: true,
+   },
+   extends: ["../../.eslintrc.json"],
+   parser: "@typescript-eslint/parser",
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+   },
+   plugins: ["react", "@typescript-eslint"],
+   rules: {
+      "react/jsx-filename-extension": [
+         "warn",
+         {
+            extensions: [".jsx", ".tsx"],
+         },
+      ],
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+   },
 };
