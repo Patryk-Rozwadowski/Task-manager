f0e6b660accac463b0ffde2d31b34f632cbababc
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-08-25 23:26:02 +0200

    feat(client): Dashboard component init

---
+++ packages/client/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface DashboardProps {}
+const Dashboard = ({}: DashboardProps): JSX.Element => {
+	return (
+		<div>
+			<p>Dashboard placeholder</p>
+		</div>
+	);
+};
+
+export default Dashboard;
