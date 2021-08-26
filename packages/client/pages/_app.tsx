2f46c002b83b1ed96842fdbfaaccc2dedb426a4c
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-08-27 00:57:52 +0200

    feat(client): init redux

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
 
 export default MyApp;
