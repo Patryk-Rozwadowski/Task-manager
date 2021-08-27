4040433c23ba48b2f6319a143003eb2706c09aee
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-08-27 23:18:08 +0200

    feat(client/redux): create initial dashboard slice

---
+++ packages/client/redux/dashboard/dashboardSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+type DashboardState = {
+	tasks: string;
+};
+
+const initialState: DashboardState = {
+	tasks: "",
+};
+
+const dashboardSlice = createSlice({
+	name: "dashboard",
+	initialState,
+	reducers: {
+		fetchTasks: (state) => {
+			state.tasks = "task";
+		},
+	},
+});
+
+export type { DashboardState };
+export const { fetchTasks } = dashboardSlice.actions;
+export const getTasks = (state: RootState) => state.dashboard.tasks;
+export { dashboardSlice };
+export default dashboardSlice.reducer;
