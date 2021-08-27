cb0798005a87c460a7d38ba1adf6adf15b90a9dd
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-08-27 23:18:49 +0200

    feat(client/redux): implemented redux actions to index.tsx

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
