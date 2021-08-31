d7d2f10148c8e6abf3cd618b4358f5324b8368a3
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-08-31 22:11:34 +0200

    refactor(server): enable cors

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap(): Promise<void> {
 	const port = process.env.SERVER_PORT || 3001;
 	const logger = new Logger("bootstrap");
 	const app = await NestFactory.create(AppModule);
+	app.enableCors();
 
 	const apiDocumentBuilder = new DocumentBuilder()
 		.setTitle("Task manager")
